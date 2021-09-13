e4dd465e9455e20afa3f37b4c7b1c26b00fb7341
= <=>
2021-09-12 21:59:17 -0600

    Probando animaciones

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+window.sr = ScrollReveal();
+
+sr.reveal(".ofertas", {
+    duration: 4000,
+    origin: 'left',
+    distance: '-100px'
+})
+
+sr.reveal(".colecciones", {
+    duration: 3000,
+    origin: 'right',
+    distance: '100px'
+})
+
+sr.reveal("header", {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '-100px'
+})
